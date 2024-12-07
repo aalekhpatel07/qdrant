90db896e58694dd305bfb5bc92b0b080b5fb6679
Aalekh Patel <aalekh.gwpeck.7998@icloud.com>
2024-12-07 13:56:48 -0600

    Download Web UI from aalekhpatel07/qdrant-web-ui
    Signed-off-by: Aalekh Patel <aalekh.gwpeck.7998@icloud.com>

--- tools/sync-web-ui.sh
+++ tools/sync-web-ui.sh
@@ -7,8 +7,9 @@ OPENAPI_FILE=${OPENAPI_DIR:-"./docs/redoc/master/openapi.json"}
 
 # Download `dist.zip` from the latest release of https://github.com/qdrant/qdrant-web-ui and unzip given folder
 
+QDRANT_WEB_UI_REPO="aalekhpatel07/qdrant-web-ui"
 # Get latest dist.zip, assume jq is installed
-DOWNLOAD_LINK=$(curl --silent "https://api.github.com/repos/qdrant/qdrant-web-ui/releases/latest" | jq -r '.assets[] | select(.name=="dist-qdrant.zip") | .browser_download_url')
+DOWNLOAD_LINK=$(curl --silent "https://api.github.com/repos/${QDRANT_WEB_UI_REPO}/releases/latest" | jq -r '.assets[] | select(.name=="dist-qdrant.zip") | .browser_download_url')
 
 if command -v wget &> /dev/null
 then
